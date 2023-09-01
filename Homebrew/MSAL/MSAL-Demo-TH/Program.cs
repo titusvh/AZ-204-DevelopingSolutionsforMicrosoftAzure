11b5c8f850c5db86fa49a760ed7ec05ca35c25de
titusvh <atlassian@titusvh.dds.nl>
2023-09-01 17:14:37 +0200

    MSAL demo get a token; MS LEARN

---
+++ Homebrew/MSAL/MSAL-Demo-TH/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+namespace az204_auth
+{
+    class Program
+    {
+        private const string _clientId = "d601d68f-702e-49c1-bf84-e0d0e95a0d3a";
+        private const string _tenantId = "efae2b71-7f88-46de-a2ef-372ed510ea49";
+
+        public static async Task Main(string[] args)
+        {
+            var app = PublicClientApplicationBuilder
+                .Create(_clientId)
+                .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+                .WithRedirectUri("http://localhost")
+                .Build();
+            string[] scopes = { "user.read" };
+            AuthenticationResult result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+
+            Console.WriteLine($"Token:\t{result.AccessToken}");
+        }
+    }
+}
